89a288e91c085b582e31dedf1638278451727551
Charlie Powell <charliepowell1@gmail.com>
2015-05-28 11:51:15 -0500

    adds routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
-var users = require('./routes/tasks');
+var tasks = require('./routes/tasks');
 
 var mongoose = require('mongoose');
 
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 //app.use('/users', users);
-app.use('/tasks', tasks);
+app.use('/api/tasks', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Task = require('../models/Tasks')
+
+/* GET /api/tasks     */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.body);
+  Task.find(function(error, tasks){
+
+    res.json(tasks);
+
+  });
 });
+/* GET /api/tasks/:id */
+router.get('/:id', function(req, res, next){
+  console.log(req.body);
+  Task.findById(req.params.id, function(error, task){
+    res.json(task);
+  });
+
+});
+
+router.post('/', function(req, res, next){
+  console.log(req.body);
+  Task.create(req.body, function(error, task){
+    res.json(task);
+  });
+
+});
+
+router.put('/:id', function(req, res, next){
+  console.log(req.body);
+  Task.findByIdAndUpdate(req.params.id, function(error, task){
+      res.json(task)
+  });
+
+});
+
+router.delete('/:id', function(req, res, next){
+  console.log(req.body);
+  Task.findByIdAndRemove(req.params.id, req.body, function(error, task){
+      res.send("Task Deleted")
+  });
+
+});
+
 
 module.exports = router;
